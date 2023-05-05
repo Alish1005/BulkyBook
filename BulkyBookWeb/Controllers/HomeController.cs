bc706a4b6a3b71dd813fd2fd40890e83ffdccda5
Alish313 <102430993+Alish313@users.noreply.github.com>
2023-05-06 02:06:45 +0300

    Add project files.

---
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDBContext _db;
+
+      
+        public HomeController(ILogger<HomeController> logger, ApplicationDBContext db)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key] //that means when we create a database the id is a primary key
+        public int id { get; set; }
+        [Required]//name is not null
+        public  string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
